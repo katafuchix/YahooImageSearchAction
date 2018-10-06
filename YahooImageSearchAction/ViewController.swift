829cc9407b12732499eb0046c250eb96e55b6b88
katafuchix <katafuchi@deskplate.net>
2018-10-06 13:41:37 +0900

    Initial Commit

---
+++ YahooImageSearchAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YahooImageSearchAction
+//
+//  Created by cano on 2018/10/06.
+//  Copyright © 2018年 cano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
